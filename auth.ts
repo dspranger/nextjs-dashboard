69bbb4c50ebc0765b91fee27dc357c28fa55d3e5
dspranger <daltonjspranger@gmail.com>
2025-08-16 23:22:39 -0500

    fixed an order of operations error on auth.ts

--- auth.ts
+++ auth.ts
@@ -30,12 +30,16 @@ export const { auth, signIn, signOut } = NextAuth({
                 if (parsedCredentials.success) {
                     const { email, password } = parsedCredentials.data;
                     const user = await getUser(email);
-                    const passwordsMatch = await bcrypt.compare(password, user.password);
 
                     if (!user) return null;
+
+                    const passwordsMatch = await bcrypt.compare(password, user.password);
+
+                    if (passwordsMatch) return user;
                 }
 
                 console.log('Invalid credentials');
+
                 return null;
             },
         }),
